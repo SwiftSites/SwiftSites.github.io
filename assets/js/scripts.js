74f72696a74abc51bc8a373e6e0f88bf3bc5a002
Sheshank Shankar <sheshank@outlook.com>
2017-12-24 00:05:31 -0800

    Add files via upload

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,15 @@
+function animate() {
+  document.getElementsByClassName("title")[0].style.display = "block";
+  document.getElementsByClassName("subtitle")[0].style.display = "block";
+  document.getElementsByClassName("title")[0].className += " animated slideInLeft";
+  document.getElementsByClassName("subtitle")[0].className += " animated slideInLeft";
+  setTimeout(function () {
+    document.getElementsByClassName("navbar-special-item")[0].className += " animated fadeInDown";
+    document.getElementsByClassName("navbar-special-item")[0].style.display = "block";
 
+    for (var i = 0; i < document.getElementsByClassName("navbar-item").length; i++) {
+      document.getElementsByClassName("navbar-item")[i].className += " animated fadeInDown";
+      document.getElementsByClassName("navbar-item")[i].style.display = "block";
+    }
+  }, 1000);
+}
